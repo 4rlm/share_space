cf5ed273de232ec21e2eecc4e663245aa6e01edf
Lina Zoo <linazooper@gmail.com>
2017-11-10 16:57:00 -0600

    css integrated and partials created for nav and main content

--- db/migrate/20171109214331_create_amenities.rb
+++ db/migrate/20171109214331_create_amenities.rb
@@ -1,22 +1,7 @@
 class CreateAmenities < ActiveRecord::Migration[5.1]
   def change
     create_table :amenities do |t|
-      t.string :accessibility
-      t.string :bike_rack
-      t.string :coffee
-      t.string :computer
-      t.string :kitchen
-      t.string :locked_door
-      t.string :monitor
-      t.string :parking
-      t.string :pet_friendly
-      t.string :phone
-      t.string :price
-      t.string :printer
-      t.string :shower
-      t.string :tv
-      t.string :wifi
-
+      t.string  :name
       t.timestamps
     end
   end
