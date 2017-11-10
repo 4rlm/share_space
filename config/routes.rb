b30783afbcb9d577a80ced984213e2f6cf08fa57
Adam Booth <4rlm@protonmail.ch>
2017-11-09 21:09:48 -0600

    created navigation partial via application.html, added bootstrap css to all index.html pages for all views.  setup bcrypt user auth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :spaces
-  resources :profiles
-  resources :desks
-  resources :bookings
-  resources :amenities
-  resources :addresses
+
   get 'home/index'
   root :to => "home#index"
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'profile/:id', to: 'users#show', as: 'profile'
+
+  resources :addresses, :amenities, :bookings, :desks, :profiles, :spaces, :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
