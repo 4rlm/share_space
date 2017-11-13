c5bc59e61cf62941a38e2f0fa982a19555848b2e
Adam Booth <4rlm@protonmail.ch>
2017-11-13 13:17:49 -0600

    created dynamic map partial and will try to pass dynamic data next.  also, using new method to get full address with @address.full_address in previous embeded map.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -17,7 +17,7 @@ class AddressesController < ApplicationController
     # @hash = Gmaps4rails.build_markers(@addresses) do |address, marker|
       # marker.lat address.latitude
       # marker.lng address.longitude
-    end
+    # end
 
   end
 
