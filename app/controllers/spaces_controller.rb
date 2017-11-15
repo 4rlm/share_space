8ab3bdb2a035c6e76e5e99799724bbef5a209a83
Adam Booth <4rlm@protonmail.ch>
2017-11-15 16:33:10 -0600

    started working on search.

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -8,13 +8,26 @@ class SpacesController < ApplicationController
   def index
     @spaces = Space.all
 
+    @q = Space.ransack(params[:q])
+    @spaces = @q.result.includes(:addresses)
+    # @spaces = @q.result(distinct: true).includes(:address, :desk)
+    # binding.pry
+
+    # desk = space.desks.first
+    # bookings = desk.bookings
+
+    # @spaces = @q.result(distinct: true).includes(:address).includes(:desk).includes(:booking)
+
+
+    # @q.build_condition
+
     # @search = Space.search(params[:q])
     # @spaces = @search.result
 
-    @q = Space.ransack(params[:q])
-    # @spaces = @q.result.includes(:addresses)
-    @spaces = @q.result(distinct: true).includes(:address)
-    # @q.build_condition
+    # @q = Space.ransack(params[:q])
+    # # @spaces = @q.result.includes(:addresses)
+    # @spaces = @q.result(distinct: true).includes(:address)
+    # # @q.build_condition
 
     # or use `to_a.uniq` to remove duplicates (can also be done in the view):
     # @people = @q.result.includes(:articles).page(params[:page]).to_a.uniq
